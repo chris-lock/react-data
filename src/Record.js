38f2eff0e0a53148cdf7daa6a893da7c7674598f
Chris Lock <lock.christopher@gmail.com>
2017-09-05 22:24:23 -0400

    better interface for collection.

--- src/Record.js
+++ src/Record.js
@@ -18,14 +18,14 @@ export type Record$Schema = {};
 
 export default class Record<Schema: Record$Schema> {
   static association: Association<Schema> = new Association;
-  static collection: Collection<Schema>;
+  static _collection: Collection<Schema>;
 
-  static ensureCollection(): Collection<Schema> {
-    return this.collection || this.setCollection();
+  static get collection(): Collection<Schema> {
+    return this._collection || this.setCollection();
   }
 
   static setCollection(): Collection<Schema> {
-    return (this.collection = new Collection((this: Record$Class<Schema>)));
+    return (this._collection = new Collection((this: Record$Class<Schema>)));
   }
 
   static asCollection(): Record$Class<Schema> {
@@ -35,19 +35,19 @@ export default class Record<Schema: Record$Schema> {
   }
 
   static first(query: Collection$Query<Schema>): void {
-    return this.ensureCollection().first(query);
+    return this.collection.first(query);
   }
 
   static where(query: Collection$Query<Schema>): void {
-    return this.ensureCollection().where(query);
+    return this.collection.where(query);
   }
 
   static add(key: WriteKey, schema: Schema): void {
-    this.ensureCollection().add(key, schema);
+    this.collection.add(key, schema);
   }
 
   static remove(key: WriteKey, query: Collection$Query<Schema>): void {
-    this.ensureCollection().remove(key, query);
+    this.collection.remove(key, query);
   }
 
   _data: Schema;
